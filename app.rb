77b80fdffbb496fd2cc9580d5a1afdf6bc0fe97d
Axel Johnsson <johnsson.axel@gmail.com>
2014-11-08 01:11:53 -0800

    Make Sinatra render index.html on catch all

--- app.rb
+++ app.rb
@@ -6,10 +6,6 @@ set :session_secret, 'some-very-large-random-value'
 
 pyro = TinderPyro::Client.new
 
-get '/' do
-  File.read(File.join('public', 'index.html'))
-end
-
 post '/sign_in' do 
  	pyro.sign_in( params[:FACEBOOK_ID], params[:FACEBOOK_TOKEN] )
  	session[:auth] = pyro.instance_variable_get(:@requestor).instance_variable_get(:@auth_token)
@@ -20,4 +16,8 @@ get '/get_nearby_users' do
 	pyro.instance_variable_get(:@requestor).instance_variable_set(:@auth_token, session[:auth])
 	x = pyro.get_nearby_users
 	"#{x}"
+end
+
+get '/*' do
+  File.read(File.join('public', 'index.html'))
 end
